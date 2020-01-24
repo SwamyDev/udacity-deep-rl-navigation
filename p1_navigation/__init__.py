f3bcfe9fdad18f33a2be39f7b581310fc4fa7d87
SwamyDev <b.raml@gmx.at>
2020-01-24 13:52:33 +0100

    Initial project skeleton setup
    It makes it easier to run tests and setup the correct python
    environment. It provides a nice starting point for implementing the
    project.

---
+++ p1_navigation/__init__.py
@@ -0,0 +1,1 @@
+from p1_navigation._version import __version__
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+here = Path(__file__).absolute().parent
+
+long_description = (here / Path('README.md')).read_text()
+
+_version = {}
+exec((here / Path('p1_navigation/_version.py')).read_text(), _version)
+
+setup(
+    name='p1_navigation',
+    version=_version['__version__'],
+    description='RL time series testbed',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/SwamyDev/udacity-deep-rl-navigation',
+    author='Bernhard Raml',
+    packages=find_packages(include=['p1_navigation', 'p1_navigation.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['udacity-unityagents'],
+    extras_require={"test": ['pytest', 'pytest-cov', 'gym-quickcheck']},
+    python_requires='>=3.6,<3.8'
+)
